b7d83aee2b940778e04a5d89b91bd1847b5e9cb2
Roopa0802 <roopa.niveditha@gmail.com>
2021-04-07 15:08:07 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Child myObj = new Child();
+        System.out.println(myObj.returnValue(1011));
+    }
+}
+
+abstract class Parent {
+
+    abstract int display(int i);
+
+    int returnValue(int i) {
+        return display(++i);
+    }
+}
+
+class Child extends Parent {
+    int display(int i) {
+        return ++i;
+    }
+}
+
